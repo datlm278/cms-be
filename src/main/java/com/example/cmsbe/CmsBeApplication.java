c1cf5c4c7ef19e2123de39001153dc458e1cf14c
minhdat2708 <minhdat01.dev@gmail.com>
2022-10-24 10:04:17 +0700

    first commit

--- src/main/java/com/example/cmsbe/CmsBeApplication.java
+++ src/main/java/com/example/cmsbe/CmsBeApplication.java
@@ -1,13 +1,33 @@
 package com.example.cmsbe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "${swagger.config.title}",
+		description = "${swagger.config.title}",
+		version = "${swagger.config.version}"),
+		servers = {@Server(url = "${swagger.config.url}")})
 public class CmsBeApplication {
 
+	private static final Logger L = LoggerFactory.getLogger(CmsBeApplication.class);
 	public static void main(String[] args) {
+
+		System.setProperty("spring.devtools.restart.enabled", "true");
+		System.setProperty("spring.profiles.default", "local");
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		//1. Run spring application
 		SpringApplication.run(CmsBeApplication.class, args);
+
+		L.info("-----------------------------------------------------------");
+		L.info("            Welcome to New CMS Service.");
+		L.info("           Application start successfully.");
+		L.info("-----------------------------------------------------------");
 	}
 
 }
