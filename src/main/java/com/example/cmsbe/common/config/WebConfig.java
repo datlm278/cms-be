fa7f3ab3cfe9f2a30428f686a07ef62c0cba20e5
minhdat2708 <minhdat01.dev@gmail.com>
2022-10-26 20:36:49 +0700

    add cors config

--- src/main/java/com/example/cmsbe/common/config/WebConfig.java
+++ src/main/java/com/example/cmsbe/common/config/WebConfig.java
@@ -1,4 +1,30 @@
 package com.example.cmsbe.common.config;
 
-public class WebConfig {
+import com.example.cmsbe.common.constant.CMSConstant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${cms.cors.origins}")
+    private String corsOrigins;
+    @Value("${cms.methods.allowed}")
+    private String allowedMethods;
+    @Value("${cms.header.allowed}")
+    private String allowedHeaders;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(CMSConstant.PREFIX_API_URL + "/**")
+                .allowCredentials(true)
+                .allowedOrigins(corsOrigins.split(","))
+                .allowedMethods(allowedMethods.split(","))
+                .allowedHeaders(allowedHeaders.split(","))
+                .maxAge(3600)
+                .exposedHeaders("Content-Disposition");
+    }
 }
